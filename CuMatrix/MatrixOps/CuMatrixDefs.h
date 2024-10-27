cc0b470c138a247135c1eef17d5c502222032586
changyu-hu <hucy19@mails.tsinghua.edu.cn>
2024-10-28 01:30:19 +0800

    bug fix

--- CuMatrix/MatrixOps/CuMatrixDefs.h
+++ CuMatrix/MatrixOps/CuMatrixDefs.h
@@ -49,9 +49,9 @@
         }                                                                                                              \
     } while (0)
 
-#define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
+#define CHECK_CUDA_ERROR(val) Check((val), #val, __FILE__, __LINE__)
 template <typename T>
-void check(T err, const char* const func, const char* const file,
+void Check(T err, const char* const func, const char* const file,
     const int line)
 {
     if (err != cudaSuccess)
